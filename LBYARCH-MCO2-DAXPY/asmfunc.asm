2f9ec0bb125f7cc2991f1e21cc538a4af4d5fdd3
oodrea <rjaemi@gmail.com>
2024-04-02 22:44:57 +0800

    yahoo!

--- LBYARCH-MCO2-DAXPY/asmfunc.asm
+++ LBYARCH-MCO2-DAXPY/asmfunc.asm
@@ -1,50 +1,34 @@
-;assembly part using x86-64
+section .data
+
 section .text
-    bits 64
-    default rel
-    global daxpyAsm
+bits 64
+default rel
+
+global daxpyAsm
+
+; Z = RCX
+; X = RDX
+; Y = R8
+; A = XMM3
+; size = 5th param (SET TO R10)
 
 daxpyAsm:
-   push rdi 
-   push r8  
-   push rbp 
-   mov rbp, rsp 
-   add rbp, 16
-        xor rbx, rbx    ; initialize for int i in for loop 
-        xor r12, r12    ; for vectorX indexing
-        xor r13, r13    ; for vectorY indexing (yIndex)
-        xor r11, r11    ; for vectorZ indexing (zIndex)
-   
-        mov r10, 10     ; i = 10
-        
-   
-        ; Function body
-        movsd xmm1, xmm0 ; Load the scalar A into XMM1
-        mov r12, rdx ; Load the address of vector X into R12
-        mov r13, r8 ; Load the address of vector Y into R13
-        mov r11, r9  ; Load the address of vector Z into R15
+    PUSH RBP
+    MOV RBP, RSP
+    ADD RBP, 16
+    MOV R10, qword[RBP+32]  ; R10 will hold the loop counter
 
-        
-perform:
-    CMP RBX, R10
-    JE END
-    movsd xmm2, [r12] ; Load the current element of X into XMM1
-    mulsd xmm2, xmm1 ; Multiply the current element of X by the scalar A
-    movsd xmm3, [r13] ; Load the current element of Y into XMM2
-    addsd xmm2, xmm3 ; Add the result to the current element of Y
-    movsd [r11], xmm2 ; Store the result in Z
+    DEC R10     ; Adjust R10 for zero-based indexing
 
-    add r12, 8 ; Move to the next element in X
-    add r13, 8 ; Move to the next element in Y
-    add r11, 8 ; Move to the next element in Z
-   
-    INC RBX
-    JMP perform
-    
-END:
-    pop rbp
-    pop r8
-    pop rdi
-    ret
+    LOOP:
+        MOVSD XMM0, QWORD [RDX + R10*8]  ; Load X[i]
+        MULSD XMM0, XMM3                 ; Multiply by A
+        ADDSD XMM0, QWORD [R8 + R10*8]    ; Add Y[i]
+        MOVSD QWORD [RCX + R10*8], XMM0   ; Store result in Z[i]
 
+        DEC R10
+        CMP R10, -1
+        JNE LOOP
 
+    POP RBP
+    RET
--- LBYARCH-MCO2-DAXPY/main.c
+++ LBYARCH-MCO2-DAXPY/main.c
@@ -1,168 +1,153 @@
+// LBYARCH S13 | x86-to-C Interface Programming Project
+// LAPUZ, Mari Salvador
+// TABADERO, Audrea Arjaemi T.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
+#include <math.h>
 
-// A->xmm0, X -> RDX, Y -> R8, Z -> R9
-extern void daxpyAsm(double A, double* X, double* Y, double* Z);
-void generateCsv(const double asmTime[], const double cTime[], int testRun, int size);
-double getAverage(double* times, int size);
-int sanityCheck(double* Z_asm, double* Z_c, int size);
-void userPrompt(int* sizeOfVector);
+#define SIZE 1048576 // 2^20
+//#define SIZE 16777216// 2^24
+//#define SIZE 536870912// 2^29
+//#define SIZE 1073741824// 2^30
 
-
-void daxpyC(double A, double* X2, double* Y2, double* Z2, int n) {
-    for (int i = 0; i < n; i++) {
-        Z2[i] = A * X2[i] + Y2[i];
-    }
-}
+int sanityCheck();
+void daxpyC(double* Z, double* X, double* Y, double A, int size);
+extern void daxpyAsm(double* Z, double* X, double* Y, double A, int size);
+void displayArray(double* Z, int size);
+void rando(double* array, int size, int seed);
+void saveToCSV(double* cData, double* asmData, int size, double cAvg, double asmAvg);
 
 int main() {
-    double asmTime[30];
-    double cTime[30];
+	int check;
 
-    clock_t start;
-    clock_t end;
+	double* cZ = (double*)malloc(SIZE * sizeof(double));
+	double* asmZ = (double*)malloc(SIZE * sizeof(double));
+	double* X = (double*)malloc(SIZE * sizeof(double));
+	double* Y = (double*)malloc(SIZE * sizeof(double));
+	double A = 4.00;
 
-    int testRun = 30;
+	double c_time_spent = 0;
+	double asm_time_spent = 0;
+	double total_c_time_spent = 0;
+	double total_asm_time_spent = 0;
+	double asmTime[30];
+	double cTime[30];
 
-    int check;
+	printf("Executing C and ASM Functions...\nPrinting first 10 elements for checking...\n\n");
+	check = sanityCheck();
+	if (check == 0) {
+		printf("Return values from ASM and C functions don't match!\n");
+		exit(0);
+	}
+	else if (check == 1) {
+		printf("Passed sanity check!\n\nExecuting tests...\n\n");
+	}
 
-    srand(time(NULL));
+	for (int i = 0; i < 30; i++) {
+		rando(X, SIZE, 1);
+		rando(Y, SIZE, 3);
 
-    double A=0;
-    int sizeOfVector=0;
-    userPrompt(&sizeOfVector, &A);
-    sizeOfVector = pow(2, sizeOfVector);
+		clock_t begin = clock();
+		daxpyC(cZ, X, Y, A, SIZE);
+		clock_t end = clock();
+		c_time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		cTime[i] = c_time_spent;
 
-    double* X = malloc(sizeOfVector * sizeof(double));
-    double* X2 = malloc(sizeOfVector * sizeof(double));
+		clock_t begin_asm = clock();
+		daxpyAsm(asmZ, X, Y, A, SIZE);
+		clock_t end_asm = clock();
+		asm_time_spent = (double)(end_asm - begin_asm) / CLOCKS_PER_SEC;
+		asmTime[i] = asm_time_spent;
 
-    double* Y = malloc(sizeOfVector * sizeof(double));
-    double* Y2 = malloc(sizeOfVector * sizeof(double));
+		total_c_time_spent += c_time_spent;
+		total_asm_time_spent += asm_time_spent;
 
-    double* Z = malloc(sizeOfVector * sizeof(double));
-    double* Z2 = malloc(sizeOfVector * sizeof(double));
+		printf("Iteration %d", i + 1);
+		printf("\nC: %.4lfs\n", c_time_spent);
+		printf("Assembly: %.4lfs\n\n", asm_time_spent);
 
-    if (X == NULL || Y == NULL) {
-        printf("Memory not allocated!\n");
-        free(X);
-        free(Y);
-        exit(0);
-    }
-    else {
-        printf("Memory allocated successfully!\n");
-    }
+	}
 
-    for (int i = 0; i < sizeOfVector; i++) {
-        X[i] = rand();
-        Y[i] = rand();
-    }
+	printf("---------------------------------------\n");
+	printf("\nAverage Execution time in C: %.4lfs\n", total_c_time_spent / 30);
+	printf("Average Execution time in Assembly: %.4lfs\n\n", total_asm_time_spent / 30);
 
-    for (int j = 0; j < sizeOfVector; j++) {
-        X2[j] = X[j];
-        Y2[j] = Y[j];
-    }
+	printf("Saving results to CSV file...\n");
+	saveToCSV(cTime, asmTime, 30, total_c_time_spent / 30, total_asm_time_spent / 30);
 
-    printf("Executing C and ASM Functions...\nPrinting first 10 elements for checking...\n\n");
-    daxpyAsm(A, X, Y, Z);
-    daxpyC(A, X2, Y2, Z2, sizeOfVector);
+	free(cZ);
+	free(asmZ);
+	free(X);
+	free(Y);
 
-    check = sanityCheck(Z, Z2, sizeOfVector);
-
-    if (check==0) {
-        printf("Return values from ASM and C functions don't match!\n");
-        exit(0);
-    }
-    else if(check==1) {
-        printf("Passed sanity check!\n\nExecuting test...\n");
-    }
-
-    printf("Running ASM Implementation 30 times...\n");
-    for (int i = 0; i < testRun; i++) {
-        start = clock();
-        daxpyAsm(A, X, Y, Z);
-        end = clock();
-        asmTime[i] = (double)(end - start) / CLOCKS_PER_SEC;
-    }
-
-    printf("Running C Implementation 30 times...\n");
-    for (int i = 0; i < testRun; i++) {
-        start = clock();
-        daxpyC(A, X2, Y2, Z2, sizeOfVector);
-        end = clock();
-        cTime[i] = (double)(end - start) / CLOCKS_PER_SEC;
-    }
-
-    printf("Saving results to CSV file...\n");
-    generateCsv(asmTime, cTime, testRun, sizeOfVector);
-
-    return 0;
+	return 0;
 }
 
+int sanityCheck() {
+	double cZ[3] = { 0.00 };
+	double asmZ[3] = { 0.00 };
+	double X[3] = { 1.00, 2.00, 3.00 };
+	double Y[3] = { 11.00, 12.00, 13.00 };
+	double A = 4.00;
 
-void userPrompt(int* sizeOfVector, double* A) {
-    int good;
-    do {
-        printf("Input size of vector: ");
-        scanf_s("%d", &good);
-        if (good < 0) {
-            printf("Invalid size! Please retry.\n");
-        }
-    } while (good < 0);
-    *sizeOfVector = good;
-    printf("Input A: ");
-    scanf_s("%lf", &A);
+	daxpyC(cZ, X, Y, A, 3);
+	daxpyAsm(asmZ, X, Y, A, 3);
+
+	printf("C results: ");
+	displayArray(cZ, 3);
+
+	printf("x86-64 results: ");
+	displayArray(asmZ, 3);
+
+	for (int i = 0; i < 3; i++) {
+		if (cZ[i] != asmZ[i]) {
+			return 0;
+		}
+	}
+	return 1;
+	printf("\n\n");
 }
 
-int sanityCheck(double* Z, double* Z2, int size) {
-    for (int i = 0; i < size; i++) {
-        if (Z[i] != Z2[i]) {
-            printf("index = %d", i);
-            printf("z = %f\n", Z[i]);
-            printf("z2 = %f\n", Z2[i]);
-            return 0;
-        }
-        if (i < 10) {
-            printf("x86-64 implementation Y[%d]: %.1f\n", i, Z[i]);
-            printf("C Implementation Y[%d]:      %.1f\n", i, Z2[i]);
-        }
-    }
-    return 1;
+void daxpyC(double* Z, double* X, double* Y, double A, int size) {
+	for (int i = 0; i < size; i++) {
+		Z[i] = A * X[i] + Y[i];
+	}
 }
 
-double getAverage(double* times, int size) {
-    double sum = 0.0;
-    for (int i = 0; i < size; i++) {
-        sum += times[i];
-    }
-    return sum / size;
-
+void displayArray(double* Z, int size) {
+	for (int i = 0; i < size; i++) {
+		if (i == size - 1)
+			printf("%.2f ", Z[i]);
+		else
+			printf("%.2f, ", Z[i]);
+	}
+	printf("\n");
 }
 
-void generateCsv(const double asmTime[], const double cTime[], int testRun, int size) {
-    FILE* file = fopen("result2.csv", "w");
-    if (file == NULL) {
-        perror("Error opening file");
-        return;
-    }
+void rando(double* array, int size, int seed) {
+	srand(((unsigned int)time(NULL)) / seed);
+	for (int i = 0; i < size; i++) {
+		array[i] = (double)(rand() % 10000 + 1) / 100;
+	}
+}
 
-    fprintf(file, "Vector Size,Iteration,Assembly Time (s), C Time (s)\n");
+void saveToCSV(double* cData, double* asmData, int size, double cAvg, double asmAvg) {
+	FILE* file = fopen("results.csv", "w");
+	if (file == NULL) {
+		printf("Error opening file %s for writing.\n", "results.csv");
+		return;
+	}
 
-    for (int i = 0; i < testRun; i++) {
-        if (i == 0) {
-            fprintf(file, "%d,", size);
-        }
-        else {
-            fprintf(file, ",");
-        }
-        printf("asm: %f\n c: %f\n\n", asmTime[i], cTime[1]);
-        fprintf(file, "%d,%f,%f\n", i + 1, asmTime[i], cTime[i]);
-    }
+	fprintf(file, "Iteration,C Execution Time (s),Assembly Execution Time (s)\n");
 
-    double asmAverage = getAverage(asmTime, testRun);
-    double cAverage = getAverage(cTime, testRun);
-    fprintf(file, "\n, , Average Time (s): %f, Average Time (s): %f\n", asmAverage, cAverage);
+	for (int i = 0; i < size; i++) {
+		fprintf(file, "%d,%.4lfs,%.4lfs\n", i + 1, cData[i], asmData[i]);
+	}
 
-    fclose(file);
+	fprintf(file, "Average,%.4lfs,%.4lfs\n", cAvg, asmAvg);
+
+	fclose(file);
 }
